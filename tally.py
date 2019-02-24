04781802942fabd1fb39c9c355f86349ff3408a8
Lyzw57 <askuczynski57@gmail.com>
2019-02-24 20:09:16 +0100

    changed the body of for loop. Using built-in functions

--- tally.py
+++ tally.py
@@ -3,12 +3,9 @@ def get_score(tally: str):
 
     for char in tally:
         player = char.lower()
-        if player not in scores.keys():
-            scores[player] = 0
-        if char.islower():
-            scores[player] += 1
-        else:
-            scores[player] -= 1
+        score = scores.get(player, 0)
+        change = 1 if char.islower() else - 1
+        scores[player] = score + change
 
     return sorted(scores.items(), key=lambda x: x[1], reverse=True)
 
